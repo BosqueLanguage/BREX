c16fb01033d5ee4d0f877e80e3ecadfe64aa55f0
Mark Marron <mark.marron@protonmail.com>
2024-02-29 12:44:08 -0500

    some path and reorg

--- src/path/path.h
+++ src/path/path.h
@@ -9,6 +9,19 @@ namespace bpath
     public:
         const std::optional<brex::ASCIIString> userinfo;
         const brex::ASCIIString host;
+
+        AuthorityInfo(std::optional<brex::ASCIIString> userinfo, brex::ASCIIString host) : userinfo(userinfo), host(host) {;}
+
+        std::u8string toBSQONFormat() const
+        {
+            std::u8string res = u8"//";
+            if(this->userinfo.has_value()) {
+                res += std::u8string(this->userinfo.value().cbegin(), this->userinfo.value().cend());
+                res += u8"@";
+            }
+            res += std::u8string(this->host.cbegin(), this->host.cend());
+            return res;
+        }
     };
 
     class ElementInfo
@@ -16,6 +29,18 @@ namespace bpath
     public:
         const brex::ASCIIString ename;
         const std::optional<brex::ASCIIString> ext;
+
+        ElementInfo(brex::ASCIIString ename, std::optional<brex::ASCIIString> ext) : ename(ename), ext(ext) {;}
+
+        std::u8string toBSQONFormat() const
+        {
+            std::u8string res = std::u8string(this->ename.cbegin(), this->ename.cend());
+            if(this->ext.has_value()) {
+                res += u8".";
+                res += std::u8string(this->ext.value().cbegin(), this->ext.value().cend());
+            }
+            return res;
+        }
     };
 
     class PathElement
@@ -24,16 +49,23 @@ namespace bpath
         const brex::ASCIIString scheme;
         const std::optional<AuthorityInfo> authorityinfo;
         const std::vector<brex::ASCIIString> segments;
-        const std::optional<ElementInfo> elementinfo;
+        const ElementInfo elementinfo;
 
-        static PathElement* jparse(json jv)
-        {
-            return nullptr;
-        }
+        PathElement(brex::ASCIIString scheme, std::optional<AuthorityInfo> authorityinfo, std::vector<brex::ASCIIString> segments, ElementInfo elementinfo) : scheme(scheme), authorityinfo(authorityinfo), segments(segments), elementinfo(elementinfo) {;}
 
-        static bool test(const std::string& path)
+        std::u8string toBSQONFormat() const
         {
-            return true;
+            std::u8string res = std::u8string(this->scheme.cbegin(), this->scheme.cend());
+            res += u8":";
+            if(this->authorityinfo.has_value()) {
+                res += this->authorityinfo.value().toBSQONFormat();
+            }
+            res += u8"/";
+            for(size_t i = 0; i < this->segments.size(); ++i) {
+                res += std::u8string(this->segments[i].cbegin(), this->segments[i].cend()) + u8'/';
+            }
+            
+            return res + this->elementinfo.toBSQONFormat();
         }
     };
 
@@ -44,14 +76,23 @@ namespace bpath
         const std::optional<AuthorityInfo> authorityinfo;
         const std::vector<brex::ASCIIString> segments;
 
-        static PathGroup* jparse(json jv)
-        {
-            return nullptr;
-        }
+        PathGroup(brex::ASCIIString scheme, std::optional<AuthorityInfo> authorityinfo, std::vector<brex::ASCIIString> segments) : scheme(scheme), authorityinfo(authorityinfo), segments(segments) {;}
 
-        static bool test(const std::string& path)
+        std::u8string toBSQONFormat() const
         {
-            return true;
+            std::u8string res = std::u8string(this->scheme.cbegin(), this->scheme.cend());
+            res += u8":";
+            if(this->authorityinfo.has_value()) {
+                res += this->authorityinfo.value().toBSQONFormat();
+            }
+            res += u8"/";
+            for(size_t i = 0; i < this->segments.size(); ++i) {
+                if(i > 0) {
+                    res += u8"/";
+                }
+                res += std::u8string(this->segments[i].cbegin(), this->segments[i].cend());
+            }
+            return res;
         }
     };
 }
--- src/path/path_glob.h
+++ src/path/path_glob.h
@@ -7,79 +7,95 @@
 
 namespace bpath
 {
-    enum class GlobComponentType
+    enum class GlobSimpleComponentTag
     {
-        LITERAL,
-        WILDCARD,
-        REGEX,
-        SEGMENT_LITERAL,
-        SEGMENT_WILDCARD,
-        SEGMENT_REGEX,
-        SEGMENT_EXPANSIVE_WILDCARD,
-        SEGMENT_EXPANSIVE_STAR,
-        SEGMENT_EXPANSIVE_PLUS,
-        SEGMENT_EXPANSIVE_RANGE,
-        SEGMENT_EXPANSIVE_QUESTION
+        LITERAL_TAG,
+        WILDCARD_TAG,
+        REGEX_TAG
     };
 
-    class GlobComponent
+    class GlobSimpleComponent
     {
     public:
-        const GlobComponentType type;
+        const GlobSimpleComponentTag type;
 
-        GlobComponent(GlobComponentType type) : type(type) {;}
-        virtual ~GlobComponent() {;}
+        GlobSimpleComponent(GlobSimpleComponentTag type) : type(type) {;}
+        virtual ~GlobSimpleComponent() {;}
 
         virtual std::u8string toBSQONFormat() const = 0;
 
-        static GlobComponent* jparse(json jv);
+        static GlobSimpleComponent* jparse(json jv);
     };
 
-    class LiteralComponent : public GlobComponent
+    class LiteralComponent : public GlobSimpleComponent
     {
     public:
         const brex::ASCIIString value;
 
-        LiteralComponent(brex::ASCIIString value) : GlobComponent(GlobComponentType::LITERAL), value(value) {;}
+        LiteralComponent(brex::ASCIIString value) : GlobSimpleComponent(GlobSimpleComponentTag::LITERAL_TAG), value(value) {;}
         virtual ~LiteralComponent() {;}
 
         std::u8string toBSQONFormat() const override final
         {
             return std::u8string(this->value.cbegin(), this->value.cend());
         }
+
+        static LiteralComponent* jparse(json jv);
     };
 
-    class WildcardComponent : public GlobComponent
+    class WildcardComponent : public GlobSimpleComponent
     {
     public:
-        WildcardComponent() : GlobComponent(GlobComponentType::WILDCARD) {;}
+        WildcardComponent() : GlobSimpleComponent(GlobSimpleComponentTag::WILDCARD_TAG) {;}
         virtual ~WildcardComponent() {;}
 
         std::u8string toBSQONFormat() const override final
         {
             return u8"*";
         }
+
+        static WildcardComponent* jparse(json jv);
     };
 
-    class RegexComponent : public GlobComponent
+    class RegexComponent : public GlobSimpleComponent
     {
     public:
         const brex::Regex* re;
 
-        RegexComponent(const brex::Regex* re) : GlobComponent(GlobComponentType::REGEX), re(re) {;}
+        RegexComponent(const brex::Regex* re) : GlobSimpleComponent(GlobSimpleComponentTag::REGEX_TAG), re(re) {;}
         virtual ~RegexComponent() {;}
 
         std::u8string toBSQONFormat() const override final
         {
             return re->toBSQONGlobFormat();
         }
+
+        static RegexComponent* jparse(json jv);
     };
 
-    class SegmentGlobCompnent : public GlobComponent
+    enum class GlobSegmentComponentTag
+    {
+        SEGMENT_LITERAL_TAG,
+        SEGMENT_WILDCARD_TAG,
+        SEGMENT_REGEX_TAG,
+        SEGMENT_EXPANSIVE_WILDCARD_TAG,
+        SEGMENT_EXPANSIVE_STAR_TAG,
+        SEGMENT_EXPANSIVE_PLUS_TAG,
+        SEGMENT_EXPANSIVE_RANGE_TAG,
+        SEGMENT_EXPANSIVE_QUESTION_TAG
+    };
+
+    class SegmentGlobCompnent
     {
     public:
-        SegmentGlobCompnent(GlobComponentType type) : GlobComponent(type) {;}
+        const GlobSegmentComponentTag tag;
+    
+        SegmentGlobCompnent(GlobSegmentComponentTag tag) : tag(tag) {;}
         virtual ~SegmentGlobCompnent() {;}
+
+        virtual std::u8string toBSQONFormat() const = 0;
+
+        static SegmentGlobCompnent* jparse(json jv);
     };
 
     class SegmentLiteralComponent : public SegmentGlobCompnent
@@ -87,25 +103,29 @@ namespace bpath
     public:
         const brex::ASCIIString value;
 
-        SegmentLiteralComponent(brex::ASCIIString value) : SegmentGlobCompnent(GlobComponentType::SEGMENT_LITERAL), value(value) {;}
+        SegmentLiteralComponent(brex::ASCIIString value) : SegmentGlobCompnent(GlobSegmentComponentTag::SEGMENT_LITERAL_TAG), value(value) {;}
         virtual ~SegmentLiteralComponent() {;}
 
         std::u8string toBSQONFormat() const override final
         {
             return std::u8string(this->value.cbegin(), this->value.cend());
         }
+
+        static SegmentLiteralComponent* jparse(json jv);
     };
     
     class SegmentWildcardComponent : public SegmentGlobCompnent
     {
     public:
-        SegmentWildcardComponent() : SegmentGlobCompnent(GlobComponentType::SEGMENT_WILDCARD) {;}
+        SegmentWildcardComponent() : SegmentGlobCompnent(GlobSegmentComponentTag::SEGMENT_WILDCARD_TAG) {;}
         virtual ~SegmentWildcardComponent() {;}
 
         std::u8string toBSQONFormat() const override final
         {
             return u8"*";
         }
+
+        static SegmentWildcardComponent* jparse(json jv);
     };
 
     class SegmentRegexComponent : public SegmentGlobCompnent
@@ -113,32 +133,36 @@ namespace bpath
     public:
         const brex::Regex* re;
 
-        SegmentRegexComponent(const brex::Regex* re) : SegmentGlobCompnent(GlobComponentType::SEGMENT_REGEX), re(re) {;}
+        SegmentRegexComponent(const brex::Regex* re) : SegmentGlobCompnent(GlobSegmentComponentTag::SEGMENT_REGEX_TAG), re(re) {;}
         virtual ~SegmentRegexComponent() {;}
 
         std::u8string toBSQONFormat() const override final
         {
             return re->toBSQONGlobFormat();
         }
+
+        static SegmentRegexComponent* jparse(json jv);
     };
 
     class SegmentExpansiveComponent : public SegmentGlobCompnent
     {
     public:
-        SegmentExpansiveComponent(GlobComponentType type) : SegmentGlobCompnent(type) {;}
+        SegmentExpansiveComponent(GlobSegmentComponentTag type) : SegmentGlobCompnent(type) {;}
         virtual ~SegmentExpansiveComponent() {;}
     };
 
     class SegmentExpansiveWildcardComponent : public SegmentExpansiveComponent
     {
     public:
-        SegmentExpansiveWildcardComponent() : SegmentExpansiveComponent(GlobComponentType::SEGMENT_EXPANSIVE_WILDCARD) {;}
+        SegmentExpansiveWildcardComponent() : SegmentExpansiveComponent(GlobSegmentComponentTag::SEGMENT_EXPANSIVE_WILDCARD_TAG) {;}
         virtual ~SegmentExpansiveWildcardComponent() {;}
 
         std::u8string toBSQONFormat() const override final
         {
             return u8"**";
         }
+
+        static SegmentExpansiveWildcardComponent* jparse(json jv);
     };
 
     class SegmentExpansiveRegexComponent : public SegmentExpansiveComponent
@@ -146,32 +170,36 @@ namespace bpath
     public:
         const brex::Regex* re;
 
-        SegmentExpansiveRegexComponent(GlobComponentType type, const brex::Regex* re) : SegmentExpansiveComponent(type), re(re) {;}
+        SegmentExpansiveRegexComponent(GlobSegmentComponentTag type, const brex::Regex* re) : SegmentExpansiveComponent(type), re(re) {;}
         virtual ~SegmentExpansiveRegexComponent() {;} 
     };
 
     class SegmentExpansiveStarComponent : public SegmentExpansiveRegexComponent
     {
     public:
-        SegmentExpansiveStarComponent(const brex::Regex* re) : SegmentExpansiveRegexComponent(GlobComponentType::SEGMENT_EXPANSIVE_STAR, re) {;}
+        SegmentExpansiveStarComponent(const brex::Regex* re) : SegmentExpansiveRegexComponent(GlobSegmentComponentTag::SEGMENT_EXPANSIVE_STAR_TAG, re) {;}
         virtual ~SegmentExpansiveStarComponent() {;}
 
         std::u8string toBSQONFormat() const override final
         {
             return re->toBSQONGlobFormat() + u8'*';
         }
+
+        static SegmentExpansiveStarComponent* jparse(json jv);
     };
 
     class SegmentExpansivePlusComponent : public SegmentExpansiveRegexComponent
     {
     public:
-        SegmentExpansivePlusComponent(const brex::Regex* re) : SegmentExpansiveRegexComponent(GlobComponentType::SEGMENT_EXPANSIVE_PLUS, re) {;}
+        SegmentExpansivePlusComponent(const brex::Regex* re) : SegmentExpansiveRegexComponent(GlobSegmentComponentTag::SEGMENT_EXPANSIVE_PLUS_TAG, re) {;}
         virtual ~SegmentExpansivePlusComponent() {;}
 
         std::u8string toBSQONFormat() const override final
         {
             return re->toBSQONGlobFormat() + u8'+';
         }
+
+        static SegmentExpansivePlusComponent* jparse(json jv);
     };
 
     class SegmentExpansiveRangeComponent : public SegmentExpansiveRegexComponent
@@ -180,7 +208,7 @@ namespace bpath
         const int64_t low;
         const int64_t high;
 
-        SegmentExpansiveRangeComponent(const brex::Regex* re, int64_t low, int64_t high) : SegmentExpansiveRegexComponent(GlobComponentType::SEGMENT_EXPANSIVE_RANGE, re), low(low), high(high) {;}
+        SegmentExpansiveRangeComponent(const brex::Regex* re, int64_t low, int64_t high) : SegmentExpansiveRegexComponent(GlobSegmentComponentTag::SEGMENT_EXPANSIVE_RANGE_TAG, re), low(low), high(high) {;}
         virtual ~SegmentExpansiveRangeComponent() {;}
 
         std::u8string toBSQONFormat() const override final
@@ -203,50 +231,104 @@ namespace bpath
             
             return re->toBSQONGlobFormat() + std::u8string(iterstr.cbegin(), iterstr.cend());
         }
+
+        static SegmentExpansiveRangeComponent* jparse(json jv);
     };
 
-    class SegmentExpansivQuestionComponent : public SegmentExpansiveRegexComponent
+    class SegmentExpansiveQuestionComponent : public SegmentExpansiveRegexComponent
     {
     public:
-        SegmentExpansivQuestionComponent(const brex::Regex* re) : SegmentExpansiveRegexComponent(GlobComponentType::SEGMENT_EXPANSIVE_QUESTION, re) {;}
-        virtual ~SegmentExpansivQuestionComponent() {;}
+        SegmentExpansiveQuestionComponent(const brex::Regex* re) : SegmentExpansiveRegexComponent(GlobSegmentComponentTag::SEGMENT_EXPANSIVE_QUESTION_TAG, re) {;}
+        virtual ~SegmentExpansiveQuestionComponent() {;}
 
         std::u8string toBSQONFormat() const override final
         {
             return re->toBSQONGlobFormat() + u8'?';
         }
+
+        static SegmentExpansiveQuestionComponent* jparse(json jv);
     };
 
-    class AuthorityInfo
+    class GlobAuthorityInfo
     {
     public:
-        const std::optional<GlobSimple*> userinfo;
-        const GlobSimple* host;
+        const std::optional<GlobSimpleComponent*> userinfo;
+        const GlobSimpleComponent* host;
+
+        GlobAuthorityInfo(std::optional<GlobSimpleComponent*> userinfo, GlobSimpleComponent* host) : userinfo(userinfo), host(host) {;}
+
+        std::u8string toBSQONFormat() const
+        {
+            if(this->userinfo.has_value()) {
+                return this->userinfo.value()->toBSQONFormat() + u8'@' + this->host->toBSQONFormat();
+            }
+            else {
+                return this->host->toBSQONFormat();
+            }
+        }
+
+        static GlobAuthorityInfo* jparse(json jv);
     };
 
-    class ElementInfo
+    class GlobElementInfo
     {
     public:
-        const GlobSimple* ename;
-        const std::optional<GlobSimple*> ext;
+        const GlobSimpleComponent* ename;
+        const std::optional<GlobSimpleComponent*> ext;
+
+        GlobElementInfo(GlobSimpleComponent* ename, std::optional<GlobSimpleComponent*> ext) : ename(ename), ext(ext) {;}
+
+        std::u8string toBSQONFormat() const
+        {
+            if(this->ext.has_value()) {
+                return this->ename->toBSQONFormat() + u8'.' + this->ext.value()->toBSQONFormat();
+            }
+            else {
+                return this->ename->toBSQONFormat();
+            }
+        }
+
+        static GlobElementInfo* jparse(json jv);
     };
 
     class PathGlob
     {
     public:
-        //
-        //TODO fill this in with code to handle path validation
-        //
+        const GlobSimpleComponent* scheme;
+        const std::optional<GlobAuthorityInfo*> authorityinfo;
+        const std::vector<SegmentGlobCompnent*> segments;
+        const std::optional<GlobElementInfo*> elementinfo;
 
-        static PathGlob* jparse(json jv)
+        const bool tailingSlash; //cannot have elenentinfo and tailingSlash as false
+
+        PathGlob(GlobSimpleComponent* scheme, std::optional<GlobAuthorityInfo*> authorityinfo, std::vector<SegmentGlobCompnent*> segments, std::optional<GlobElementInfo*> elementinfo, bool tailingStar) : scheme(scheme), authorityinfo(authorityinfo), segments(segments), elementinfo(elementinfo), tailingSlash(tailingSlash) {;}
+
+        std::u8string toBSQONFormat() const
         {
-            return nullptr;
+            std::u8string res = this->scheme->toBSQONFormat() + u8":";
+            if(this->authorityinfo.has_value()) {
+                res += u8"//" + this->authorityinfo.value()->toBSQONFormat();
+            }
+            res += u8"/";
+            for(size_t i = 0; i < this->segments.size(); ++i) {
+                res += this->segments[i]->toBSQONFormat();
+                
+                if(i != this->segments.size() - 1) {
+                    res += u8"/";
+                }
+            }
+            
+            if(tailingSlash) {
+                res += u8"/";
+            }
+            if(this->elementinfo.has_value()) {
+                res += this->elementinfo.value()->toBSQONFormat();
+            }
+            
+            return res;
         }
 
-        static bool test(const std::string& path)
-        {
-            return true;
-        }
+        static PathGlob* jparse(json jv);
     };
 
     typedef PathGlob ResourceDescriptor;
