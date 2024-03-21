6855676c03fde32eeed434b06e53c0a785f19cbe
Mark Marron <mark.marron@protonmail.com>
2024-03-21 10:17:14 -0400

    path cleanup

--- src/path/path.h
+++ src/path/path.h
@@ -49,27 +49,14 @@ namespace bpath
         const brex::ASCIIString scheme;
         const std::optional<AuthorityInfo> authorityinfo;
         const std::vector<brex::ASCIIString> segments;
+        const std::optional<ElementInfo> elementinfo;
 
-        Path(brex::ASCIIString scheme, std::optional<AuthorityInfo> authorityinfo, std::vector<brex::ASCIIString> segments) : scheme(scheme), authorityinfo(authorityinfo), segments(segments) {;}
-        virtual ~Path() {;}
+        const bool tailingslash; //cannot have elementinfo and tailingSlash as false
 
-        virtual bool isElementPath() const { return false; }
-        virtual bool isGroupPath() const { return false; }
+        Path(brex::ASCIIString scheme, std::optional<AuthorityInfo> authorityinfo, std::vector<brex::ASCIIString> segments, std::optional<ElementInfo> elementinfo, bool tailingslash) : scheme(scheme), authorityinfo(authorityinfo), segments(segments), elementinfo(elementinfo), tailingslash(tailingslash) {;}
+        ~Path() {;}
 
-        virtual std::u8string toBSQONFormat() const = 0;
-    };
-
-    class PathElement : public Path
-    {
-    public:
-        const ElementInfo elementinfo;
-
-        PathElement(brex::ASCIIString scheme, std::optional<AuthorityInfo> authorityinfo, std::vector<brex::ASCIIString> segments, ElementInfo elementinfo) : Path(scheme, authorityinfo, segments), elementinfo(elementinfo) {;}
-        virtual ~PathElement() {;}
-
-        bool isElementPath() const override final { return true; }
-
-        std::u8string toBSQONFormat() const override final
+        std::u8string toBSQONFormat() const
         {
             std::u8string res = std::u8string(this->scheme.cbegin(), this->scheme.cend());
             res += u8":";
@@ -78,35 +65,20 @@ namespace bpath
             }
             res += u8"/";
             for(size_t i = 0; i < this->segments.size(); ++i) {
-                res += std::u8string(this->segments[i].cbegin(), this->segments[i].cend()) + u8'/';
-            }
-            
-            return res + this->elementinfo.toBSQONFormat();
-        }
-    };
-
-    class PathGroup : public Path
-    {
-    public:
-        PathGroup(brex::ASCIIString scheme, std::optional<AuthorityInfo> authorityinfo, std::vector<brex::ASCIIString> segments) : Path(scheme, authorityinfo, segments) {;}
-        virtual ~PathGroup() {;}
-
-        bool isGroupPath() const override final { return true; }
-
-        std::u8string toBSQONFormat() const override final
-        {
-            std::u8string res = std::u8string(this->scheme.cbegin(), this->scheme.cend());
-            res += u8":";
-            if(this->authorityinfo.has_value()) {
-                res += this->authorityinfo.value().toBSQONFormat();
-            }
-            res += u8"/";
-            for(size_t i = 0; i < this->segments.size(); ++i) {
-                if(i > 0) {
+                res += std::u8string(this->segments[i].cbegin(), this->segments[i].cend());
+                
+                if(i != this->segments.size() - 1) {
                     res += u8"/";
                 }
-                res += std::u8string(this->segments[i].cbegin(), this->segments[i].cend());
             }
+            
+            if(tailingslash) {
+                res += u8"/";
+            }
+            if(this->elementinfo.has_value()) {
+                res += u8"/" + this->elementinfo.value().toBSQONFormat();
+            }
+            
             return res;
         }
     };
